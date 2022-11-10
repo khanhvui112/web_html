0cf9fe181f205d39d8ea3826f61c42b292f301fa
khanhvui112 <sanvankhanh@gmail.com>
2022-11-10 17:06:39 +0700

    update color

--- js/app.js
+++ js/app.js
@@ -8,9 +8,6 @@ $(document).ready(function () {
         let text = $('#name').val().toString();
         $('.text-change').text(text+''.toUpperCase());
     });
-    $('.btn-remove-text').on('click', function () {
-        $('.title').remove()
-    });
     $('.btn-hide').on('click', function () {
         $('.btn-left').remove()
     });
