827daf0041dd666d6fc1c74f54b6481c0b00b713
khanhvui112 <sanvankhanh@gmail.com>
2022-11-10 16:27:14 +0700

    update color

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     $('.btn-reset').on('click', function () {
+        alert('Chào')
         $('body > article').remove();
         $('.btn-left').remove();
     });
     $('.btn-change-text').on('click', function () {
+        alert('Chào2')
         let text = $('#name').val();
         $('body > article').remove();
         $('.title > h1').text(text);
